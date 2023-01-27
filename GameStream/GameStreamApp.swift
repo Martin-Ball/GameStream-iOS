2e288570a9cc56ff5da21ac0f33a44745eafc286
Martin Ballester <mballester@q4tech.com>
2023-01-26 21:53:30 -0300

    Initial Commit

---
+++ GameStream/GameStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameStreamApp.swift
+//  GameStream
+//
+//  Created by Martin Ballester on 26/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct GameStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
