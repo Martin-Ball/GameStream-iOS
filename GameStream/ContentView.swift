34eff69ce0673610f7c56c9b1862997a104c2b9d
Martin Ballester <mballester@q4tech.com>
2023-01-29 18:50:24 -0300

    SignOut view done

--- GameStream/ContentView.swift
+++ GameStream/ContentView.swift
@@ -161,10 +161,15 @@ struct signInView:View{
 }
 
 struct signUpView:View{
+    @State var email = ""
+    @State var password = ""
+    @State var confirmPassword = ""
+    
     var body:some View {
         
         ScrollView{
             
+            //Photograph
             VStack(alignment: .center){
                 
                 Text("Elije una foto de perfil")
@@ -186,99 +191,120 @@ struct signUpView:View{
                         
                         Image(systemName: "camera")
                             .foregroundColor(.white)
-        
+                        
                     }
-                })
+                }).padding(.bottom)
             }
             
-//            VStack(alignment: .leading){
-//                Text("Correo electronico").foregroundColor(Color("dark-cian"))
-//
-//                ZStack(alignment: .leading){
-//
-//                    if email.isEmpty {
-//                        Text(verbatim: "ejemplo@gmail.com")
-//                            .font(.caption)
-//                            .foregroundColor(.gray)
-//                    }
-//
-//                    TextField("", text: $email)
-//                }
-//
-//                Divider().frame(height: 1)
-//                    .background(Color("dark-cian"))
-//                    .padding(.bottom)
-//
-//                Text("Contraseña")
-//                    .foregroundColor(.white)
-//
-//                ZStack(alignment: .leading){
-//
-//                    if password.isEmpty {
-//                        Text("Escribe tu contraseña").font(.caption)
-//                            .foregroundColor(.gray)
-//                    }
-//
-//                    TextField("", text: $password)
-//                }
-//
-//                Divider().frame(height: 1).background(Color("dark-cian")).padding(.bottom)
-//
-//                Text("¿Olvidaste tu contraseña?").font(.footnote).frame(width: 300, alignment: .trailing)
-//                    .foregroundColor(Color("dark-cian"))
-//                    .padding(.bottom)
-//
-//                Button(action: login, label: {
-//                    Text("INICIAR SESION")
-//                        .fontWeight(.bold)
-//                        .foregroundColor(.white)
-//                        .frame(maxWidth: .infinity, alignment: .center)
-//                        .padding(EdgeInsets(top: 11, leading: 18, bottom: 11, trailing: 18))
-//                        .overlay(RoundedRectangle(cornerRadius: 6.0).stroke(Color("dark-cian"), lineWidth: 1.0).shadow(color: .white, radius: 6))
-//                })
-//
-//                VStack{
-//
-//
-//                    Text("Inicia sesion con redes sociales")
-//                        .font(.subheadline)
-//                        .foregroundColor(.white)
-//                        .padding(.top, 70)
-//                        .padding(.bottom, 25)
-//
-//                    HStack{
-//
-//                        Spacer()
-//
-//                        Button(action: login, label: {
-//                            Text("Facebook")
-//                                .fontWeight(.bold)
-//                                .foregroundColor(.white)
-//                                .frame(maxWidth: 125, alignment: .center)
-//                                .padding(EdgeInsets(top: 9, leading: 9, bottom: 9, trailing: 15))
-//                                .background(RoundedRectangle(cornerRadius: 6.0).foregroundColor(Color("blue-gray")))
-//                        })
-//
-//                        Spacer()
-//
-//                        Button(action: login, label: {
-//                            Text("Twitter")
-//                                .fontWeight(.bold)
-//                                .foregroundColor(.white)
-//                                .frame(maxWidth: 125, alignment: .center)
-//                                .padding(EdgeInsets(top: 9, leading: 9, bottom: 9, trailing: 15))
-//                                .background(RoundedRectangle(cornerRadius: 6.0).foregroundColor(Color("blue-gray")))
-//                        })
-//
-//                        Spacer()
-//
-//                    }
-//                }
-//            }
-        }.padding(.horizontal, 77.0)
-        
-        Text("VISTA DE INICIO DE SESION")
-        
+            VStack(alignment: .leading){
+                
+                VStack(alignment: .leading){
+                    Text("Correo electronico*").foregroundColor(Color("dark-cian"))
+                    
+                    ZStack(alignment: .leading){
+                        
+                        if email.isEmpty {
+                            Text(verbatim: "ejemplo@gmail.com")
+                                .font(.caption)
+                                .foregroundColor(.gray)
+                        }
+                        
+                        TextField("", text: $email)
+                    }
+                    
+                    Divider().frame(height: 1)
+                        .background(Color("dark-cian"))
+                        .padding(.bottom)
+                    
+                    Text("Contraseña*")
+                        .foregroundColor(.white)
+                    
+                    ZStack(alignment: .leading){
+                        
+                        if password.isEmpty {
+                            Text("Escribe tu contraseña").font(.caption)
+                                .foregroundColor(.gray)
+                        }
+                        
+                        TextField("", text: $password)
+                    }
+                    
+                    Divider().frame(height: 1).background(Color("dark-cian")).padding(.bottom)
+                    
+                    
+                    Text("Confirmar contraseña*")
+                        .foregroundColor(.white)
+                    
+                    ZStack(alignment: .leading){
+                        
+                        if confirmPassword.isEmpty {
+                            Text("Escribe nuevamente tu contraseña").font(.caption)
+                                .foregroundColor(.gray)
+                        }
+                        
+                        SecureField("", text: $confirmPassword)
+                    }
+                    
+                    Divider().frame(height: 1).background(Color("dark-cian")).padding(.bottom)
+                    
+                    
+                    Text("¿Olvidaste tu contraseña?").font(.footnote).frame(width: 300, alignment: .trailing)
+                        .foregroundColor(Color("dark-cian"))
+                        .padding(.bottom)
+                    
+                }
+                
+                
+                Button(action: signUp, label: {
+                    Text("REGISTRATE")
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        .padding(EdgeInsets(top: 11, leading: 18, bottom: 11, trailing: 18))
+                        .overlay(RoundedRectangle(cornerRadius: 6.0).stroke(Color("dark-cian"), lineWidth: 1.0).shadow(color: .white, radius: 6))
+                })
+                
+                VStack{
+                    
+                    
+                    Text("Registrate con redes sociales")
+                        .font(.subheadline)
+                        .foregroundColor(.white)
+                        .padding(.top, 40)
+                        .padding(.bottom, 25)
+                    
+                    HStack{
+                        
+                        Spacer()
+                        
+                        Button(action: login, label: {
+                            Text("Facebook")
+                                .fontWeight(.bold)
+                                .foregroundColor(.white)
+                                .frame(maxWidth: 125, alignment: .center)
+                                .padding(EdgeInsets(top: 9, leading: 9, bottom: 9, trailing: 15))
+                                .background(RoundedRectangle(cornerRadius: 6.0).foregroundColor(Color("blue-gray")))
+                        })
+                        
+                        Spacer()
+                        
+                        Button(action: login, label: {
+                            Text("Twitter")
+                                .fontWeight(.bold)
+                                .foregroundColor(.white)
+                                .frame(maxWidth: 125, alignment: .center)
+                                .padding(EdgeInsets(top: 9, leading: 9, bottom: 9, trailing: 15))
+                                .background(RoundedRectangle(cornerRadius: 6.0).foregroundColor(Color("blue-gray")))
+                        })
+                        
+                        Spacer()
+                        
+                    }
+                }
+            }.padding(.horizontal, 77.0)
+            
+            Text("VISTA DE INICIO DE SESION")
+        }
     }
 }
 
@@ -297,4 +323,8 @@ func login(){
 
 func takePhoto(){
     print("Voy a tomar fotografia de perfil")
+}
+
+func signUp(){
+    print("Registrandose")
 }
