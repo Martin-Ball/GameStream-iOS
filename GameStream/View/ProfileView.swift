e9db23806a58ca0b37edcbb900e6fe7a0b94d4f5
Martin Ballester <mballester@q4tech.com>
2023-02-06 23:03:17 -0300

    profile in progress

--- GameStream/View/ProfileView.swift
+++ GameStream/View/ProfileView.swift
@@ -8,13 +8,117 @@
 import SwiftUI
 
 struct ProfileView: View {
+    
+    @State var userName = "Martin"
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Color("marine-blue").ignoresSafeArea()
+                .navigationBarHidden(true)
+                .navigationBarBackButtonHidden(true)
+            
+            VStack{
+                
+                Text("Perfil")
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                    .frame(minWidth: 0, idealWidth: 100, maxWidth: .infinity,
+                           alignment: .center)
+                    .padding()
+                
+                VStack{
+                    Image("perfilEjemplo")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 118, height: 118)
+                        .clipShape(Circle())
+                }.padding(EdgeInsets(top:16, leading:0, bottom: 32, trailing: 0))
+                
+                Text("AJUSTES")
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                    .frame(minWidth: 0, idealWidth: 100, maxWidth: .infinity,
+                           alignment: .leading)
+                    .padding(.leading, 18)
+                
+                SettingsModule()
+                    
+            }
+        }.onAppear(
+        
+            perform: {
+                print("Revisando informacion del usuario")
+            }
+            
+        )
     }
 }
 
 struct ProfileView_Previews: PreviewProvider {
     static var previews: some View {
         ProfileView()
+    }
+}
+
+struct SettingsModule : View {
+    
+    @State var isToggleOn : Bool = true
+    @State var isEditProfileViewActive = false
+    
+    var body : some View {
+        VStack{
+            
+            Button(action: {}, label: {
+                HStack{
+                    Text("Cuenta")
+                        .foregroundColor(.white)
+                    
+                    Spacer()
+                    
+                    Text(">").foregroundColor(.white)
+                }.padding()
+            }).background(Color("blue-gray"))
+                .clipShape(RoundedRectangle(cornerRadius: 1.0))
+            
+            Button(action: {}, label: {
+                HStack{
+                    Text("Notificaciones")
+                        .foregroundColor(.white)
+                    
+                    Spacer()
+                    
+                    Toggle("", isOn: $isToggleOn)
+                }.padding()
+            }).background(Color("blue-gray"))
+                .clipShape(RoundedRectangle(cornerRadius: 1.0))
+            
+            Button(action: {
+                isEditProfileViewActive = true
+            }, label: {
+                HStack{
+                    Text("Editar perfil")
+                        .foregroundColor(.white)
+                    
+                    Spacer()
+                    
+                    Text(">").foregroundColor(.white)
+                }.padding()
+            }).background(Color("blue-gray"))
+                .clipShape(RoundedRectangle(cornerRadius: 1.0))
+            
+            Button(action: {}, label: {
+                HStack{
+                    Text("Califica esta aplicacion")
+                        .foregroundColor(.white)
+                    
+                    Spacer()
+                    
+                    Text(">").foregroundColor(.white)
+                }.padding()
+            }).background(Color("blue-gray"))
+                .clipShape(RoundedRectangle(cornerRadius: 1.0))
+            
+        }
     }
 }
